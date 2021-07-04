9e8a8711a732d24dbb6d3ae05e4952b7e8a836df
iamjumana <85646609+iamjumana@users.noreply.github.com>
2021-07-04 21:19:21 +0300

    Add files via upload

--- conect.php
+++ conect.php
@@ -11,13 +11,13 @@
   echo "Connected successfully <br>";
   $conn->close();}}
   
-  $sql = "SELECT id, firstname, lastname FROM MyGuests";
+  $sql = "one,two,three,four,five,six";
 $result = mysqli_query($conn, $sql);
 
 if (mysqli_num_rows($result) > 0) {
   // output data of each row
   while($row = mysqli_fetch_assoc($result)) {
-    echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+    echo "id: " . $row["one"]. " - Name: " . $row["two"]. " " . $row["three"]." ". $row["four"]." ".$row["five"]." ".$row["six"]. "<br>";
   }
 } else {
   echo "0 results";
